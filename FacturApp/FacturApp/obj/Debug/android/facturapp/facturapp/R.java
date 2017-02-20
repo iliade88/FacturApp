07ebe34500b820c9c22ef03fc8f55f3daaa5faf4
Mario Navarro <mnr16@alu.ua.es>
2017-02-20 19:43:24 +0100

    Añadida pantalla copia de seguridad

--- FacturApp/FacturApp/obj/Debug/android/facturapp/facturapp/R.java
+++ FacturApp/FacturApp/obj/Debug/android/facturapp/facturapp/R.java
@@ -14,24 +14,25 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050009;
-        public static final int button2=0x7f05000b;
+        public static final int button1=0x7f050003;
+        public static final int button2=0x7f050004;
         public static final int button3=0x7f05000c;
         public static final int button4=0x7f05000d;
-        public static final int editText1=0x7f050003;
-        public static final int editText2=0x7f050007;
-        public static final int imageButton1=0x7f05000a;
+        public static final int editText1=0x7f050006;
+        public static final int editText2=0x7f050009;
+        public static final int imageButton1=0x7f05000b;
         public static final int linearLayout1=0x7f050000;
-        public static final int linearLayout2=0x7f050002;
-        public static final int linearLayout3=0x7f050004;
-        public static final int linearLayout4=0x7f050006;
-        public static final int linearLayout5=0x7f050008;
-        public static final int textView1=0x7f050001;
-        public static final int textView2=0x7f050005;
+        public static final int linearLayout2=0x7f050001;
+        public static final int linearLayout3=0x7f050002;
+        public static final int linearLayout4=0x7f050008;
+        public static final int linearLayout5=0x7f05000a;
+        public static final int textView1=0x7f050005;
+        public static final int textView2=0x7f050007;
     }
     public static final class layout {
-        public static final int login=0x7f030000;
-        public static final int pantallaprincipal=0x7f030001;
+        public static final int copiaseguridad=0x7f030000;
+        public static final int login=0x7f030001;
+        public static final int pantallaprincipal=0x7f030002;
     }
     public static final class string {
         public static final int ApplicationName=0x7f040001;
