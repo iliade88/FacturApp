7edeb8ec6d8187114eed55aa3a27330b34f378dd
Cristobal Jesus <cris_j_g@hotmail.com>
2017-02-20 20:06:56 +0100

    Pantalla de editar cliente

--- FacturApp/FacturApp/Resources/Resource.Designer.cs
+++ FacturApp/FacturApp/Resources/Resource.Designer.cs
@@ -60,53 +60,56 @@ namespace FacturApp
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int button1 = 2131034114;
-			
-			// aapt resource value: 0x7f050003
-			public const int button2 = 2131034115;
-			
-			// aapt resource value: 0x7f050004
-			public const int button3 = 2131034116;
-			
-			// aapt resource value: 0x7f050005
-			public const int button4 = 2131034117;
-			
-			// aapt resource value: 0x7f050007
-			public const int editText1 = 2131034119;
-			
-			// aapt resource value: 0x7f050009
-			public const int editText2 = 2131034121;
-			
 			// aapt resource value: 0x7f05000b
-			public const int editText3 = 2131034123;
+			public const int button1 = 2131034123;
 			
 			// aapt resource value: 0x7f05000d
-			public const int editText4 = 2131034125;
+			public const int button2 = 2131034125;
+			
+			// aapt resource value: 0x7f05000e
+			public const int button3 = 2131034126;
 			
 			// aapt resource value: 0x7f05000f
-			public const int editText5 = 2131034127;
+			public const int button4 = 2131034127;
 			
-			// aapt resource value: 0x7f050001
-			public const int imageButton1 = 2131034113;
+			// aapt resource value: 0x7f050002
+			public const int editText1 = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int editText2 = 2131034116;
+			
+			// aapt resource value: 0x7f050006
+			public const int editText3 = 2131034118;
+			
+			// aapt resource value: 0x7f050010
+			public const int editText4 = 2131034128;
+			
+			// aapt resource value: 0x7f05000a
+			public const int editText5 = 2131034122;
+			
+			// aapt resource value: 0x7f050008
+			public const int editText6 = 2131034120;
+			
+			// aapt resource value: 0x7f05000c
+			public const int imageButton1 = 2131034124;
 			
 			// aapt resource value: 0x7f050000
 			public const int linearLayout1 = 2131034112;
 			
-			// aapt resource value: 0x7f050006
-			public const int textView1 = 2131034118;
+			// aapt resource value: 0x7f050001
+			public const int textView1 = 2131034113;
 			
-			// aapt resource value: 0x7f050008
-			public const int textView2 = 2131034120;
+			// aapt resource value: 0x7f050003
+			public const int textView2 = 2131034115;
 			
-			// aapt resource value: 0x7f05000a
-			public const int textView3 = 2131034122;
+			// aapt resource value: 0x7f050005
+			public const int textView3 = 2131034117;
 			
-			// aapt resource value: 0x7f05000c
-			public const int textView4 = 2131034124;
+			// aapt resource value: 0x7f050007
+			public const int textView4 = 2131034119;
 			
-			// aapt resource value: 0x7f05000e
-			public const int textView5 = 2131034126;
+			// aapt resource value: 0x7f050009
+			public const int textView5 = 2131034121;
 			
 			static Id()
 			{
@@ -122,10 +125,13 @@ namespace FacturApp
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int PantallaPrincipal = 2130903040;
+			public const int EditarUsuario = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Registro = 2130903041;
+			public const int PantallaPrincipal = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int Registro = 2130903042;
 			
 			static Layout()
 			{
