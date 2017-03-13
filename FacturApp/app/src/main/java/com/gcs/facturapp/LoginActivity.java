5b1f1128d4632d19195de9b92990f7b13e6c7bc1
Cristobal Jesus <cris_j_g@hotmail.com>
2017-03-13 18:25:27 +0100

    Renombrada actividad registro

--- FacturApp/app/src/main/java/com/gcs/facturapp/LoginActivity.java
+++ FacturApp/app/src/main/java/com/gcs/facturapp/LoginActivity.java
@@ -1,41 +1,16 @@
 package com.gcs.facturapp;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.SpannableString;
-import android.text.TextUtils;
 import android.text.style.UnderlineSpan;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
@@ -80,7 +55,7 @@ public class LoginActivity extends AppCompatActivity {
                 {
                     case R.id.link_registro:
 
-                        Intent intent = new Intent(view.getContext(), Registro.class);
+                        Intent intent = new Intent(view.getContext(), RegistroActivity.class);
                         startActivity(intent);
                         break;
                 }
--- FacturApp/app/src/main/java/com/gcs/facturapp/RegistroActivity.java
+++ FacturApp/app/src/main/java/com/gcs/facturapp/RegistroActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class Registro extends AppCompatActivity {
+public class RegistroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
