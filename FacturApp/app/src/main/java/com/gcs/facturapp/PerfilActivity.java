882dd41dd4394b7220e962706f8d6bd33806723d
alberto sapiña mora <alberto sapia mora>
2017-03-26 17:59:00 +0200

    Coger imagen de galería

--- FacturApp/app/src/main/java/com/gcs/facturapp/PerfilActivity.java
+++ FacturApp/app/src/main/java/com/gcs/facturapp/PerfilActivity.java
@@ -1,10 +1,13 @@
 package com.gcs.facturapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
@@ -32,6 +35,7 @@ public class PerfilActivity extends AppCompatActivity {
     private TextView apellidos;
     private EditText nuevos_apellidos;
     private ViewSwitcher switcher_botones_editar;
+    private ImageButton imagenPerfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +71,14 @@ public class PerfilActivity extends AppCompatActivity {
         nombre_empresa.setText("Viajesta S.A.");
         nombre.setText("Mario");
         apellidos.setText("Navarro Ruiz");
+
+        imagenPerfil = (ImageButton) findViewById(R.id.imagenPerfil);
+        imagenPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
     }
 
     public void onClickEditarPerfil(View v)
@@ -134,5 +146,21 @@ public class PerfilActivity extends AppCompatActivity {
             edit.setText(text.getText());
         else
             text.setText(edit.getText());
+    }
+
+    private void openGallery() {
+        Intent gallery =
+                new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 100) {
+            Uri imageUri = data.getData();
+            imagenPerfil.setImageURI(imageUri);
+        }
     }
 }
