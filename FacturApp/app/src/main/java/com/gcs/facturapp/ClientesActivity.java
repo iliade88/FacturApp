1b545d7da2878b474df1a5c39910142a3d00a52e
Mario Navarro <mnr16@alu.ua.es>
2017-03-26 19:10:59 +0200

    Añadido crear cliente

--- FacturApp/app/src/main/java/com/gcs/facturapp/ClientesActivity.java
+++ FacturApp/app/src/main/java/com/gcs/facturapp/ClientesActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -13,11 +14,13 @@ import com.gcs.facturapp.adapters.ClienteAdapter;
 import com.gcs.facturapp.models.Cliente;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class ClientesActivity extends AppCompatActivity {
 
     private ListView lista_clientes;
     private ArrayList<Cliente> clientes;
+    ClienteAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +28,26 @@ public class ClientesActivity extends AppCompatActivity {
 
         lista_clientes = (ListView) findViewById(R.id.lista_clientes);
 
-        clientes = new ArrayList<Cliente>();
-        clientes.add(new Cliente("Mario", "Navarro ruiz", "59343928J", "Calle de elda", "65676543", "mario@viajesta.com"));
-        clientes.add(new Cliente("Alberto", "Sapiña Mora", "59343928J", "Calle de elda", "965692617", "mario@viajesta.com"));
-        clientes.add(new Cliente("Cristobal", "Jesus Gonzalez", "59343928J", "Calle de elda", "96543421", "mario@viajesta.com"));
-        clientes.add(new Cliente("Manuel", "Volteador Garcia", "59343928J", "Calle de elda", "67654356", "mario@viajesta.com"));
+        Cliente clien;
 
-        ClienteAdapter adapter = new ClienteAdapter(this, clientes);
+        try{
+            clientes = (ArrayList<Cliente>) getIntent().getExtras().getSerializable("listaclientes");
+        } catch(Exception e) {
+            clientes = new ArrayList<Cliente>();
+        }
+
+        /*try{
+            clien = (Cliente) getIntent().getExtras().getSerializable("nuevocliente");
+            if(clien!=null)
+            {
+                clientes.add(clien);
+            }
+        } catch(Exception e) {
+
+        }*/
+
+
+        adapter = new ClienteAdapter(this, clientes);
         lista_clientes.setAdapter(adapter);
         lista_clientes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -47,9 +63,18 @@ public class ClientesActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), CrearClienteActivity.class);
+                intent.putExtra("listaclientes", clientes);
                 startActivity(intent);
+                finish();
             }
         });
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        adapter.notifyDataSetChanged();
     }
 
 }
--- FacturApp/app/src/main/java/com/gcs/facturapp/CrearClienteActivity.java
+++ FacturApp/app/src/main/java/com/gcs/facturapp/CrearClienteActivity.java
@@ -1,14 +1,24 @@
 package com.gcs.facturapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.gcs.facturapp.models.Cliente;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CrearClienteActivity extends AppCompatActivity {
 
     private EditText nombre_cliente;
+    private EditText apellidos_cliente;
     private EditText dnicif_cliente;
     private EditText direccion_cliente;
     private EditText telefono_cliente;
@@ -16,18 +26,14 @@ public class CrearClienteActivity extends AppCompatActivity {
     private Button boton_cancelar_crear_cliente;
     private Button boton_crear_cliente;
 
+    ArrayList<Cliente> clienteslist;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_cliente);
-
-        nombre_cliente = (EditText) findViewById(R.id.nombre_cliente);
-        dnicif_cliente = (EditText) findViewById(R.id.dnicif_cliente);
-        direccion_cliente = (EditText) findViewById(R.id.direccion_cliente);
-        telefono_cliente = (EditText) findViewById(R.id.telefono_cliente);
-        email_cliente = (EditText) findViewById(R.id.email_cliente);
-        boton_cancelar_crear_cliente = (Button) findViewById(R.id.boton_cancelar_crear_cliente);
-        boton_crear_cliente = (Button) findViewById(R.id.boton_crear_cliente);
+        clienteslist = (ArrayList<Cliente>) getIntent().getExtras().getSerializable("listaclientes");
+        Log.d("crear cliente", clienteslist.size()+"");
     }
 
     public void onClickCancelar(View view)
@@ -35,6 +41,10 @@ public class CrearClienteActivity extends AppCompatActivity {
         switch (view.getId())
         {
             case R.id.boton_cancelar_crear_cliente:
+                Intent intent = new Intent(view.getContext(), ClientesActivity.class);
+                //intent.putExtra("nuevocliente", cli);
+                intent.putExtra("listaclientes", clienteslist);
+                startActivity(intent);
                 finish();
                 break;
         }
@@ -42,12 +52,143 @@ public class CrearClienteActivity extends AppCompatActivity {
 
     public void onClickCrearCliente(View view)
     {
+        nombre_cliente = (EditText) findViewById(R.id.nombre_cliente);
+        String nombre = nombre_cliente.getText().toString().trim();
+        apellidos_cliente = (EditText) findViewById(R.id.apellidos_cliente);
+        String apellidos = apellidos_cliente.getText().toString().trim();
+        dnicif_cliente = (EditText) findViewById(R.id.dnicif_cliente);
+        String dni = dnicif_cliente.getText().toString().trim();
+        direccion_cliente = (EditText) findViewById(R.id.direccion_cliente);
+        String direccion = direccion_cliente.getText().toString().trim();
+        telefono_cliente = (EditText) findViewById(R.id.telefono_cliente);
+        String telefono = telefono_cliente.getText().toString().trim();
+        email_cliente = (EditText) findViewById(R.id.email_cliente);
+        String email = email_cliente.getText().toString().trim();
+        boton_cancelar_crear_cliente = (Button) findViewById(R.id.boton_cancelar_crear_cliente);
+        boton_crear_cliente = (Button) findViewById(R.id.boton_crear_cliente);
+
+        boolean hay_errores = false;
+
         /*TODO - Pasar datos a ClientesActivity para añadirlo a la lista*/
         switch (view.getId())
         {
             case R.id.boton_crear_cliente:
-                finish();
+                if(nombre.length()==0)
+                {
+                    nombre_cliente.setError("El nombre no puede estar vacío");
+                    hay_errores=true;
+                }
+
+                if(apellidos.length()==0)
+                {
+                    apellidos_cliente.setError("Los apellidos no pueden estar vacíos");
+                    hay_errores=true;
+                }
+
+                if(!validarDNICIF(dni))
+                {
+                    dnicif_cliente.setError("DNI o CIF inválido");
+                    hay_errores=true;
+                }
+
+                if(direccion.length()==0)
+                {
+                    direccion_cliente.setError("La dirección no puede estar vacía");
+                    hay_errores=true;
+                }
+
+                if(telefono.length()!=9 || !validarTFNO(telefono))
+                {
+                    telefono_cliente.setError("Debes introducir un teléfono válido");
+                    hay_errores=true;
+                }
+
+                if(!validarEmail(email))
+                {
+                    email_cliente.setError("El email introducido es incorrecto.");
+                    hay_errores=true;
+                }
+
+                if (hay_errores)
+                {
+                    Toast.makeText(view.getContext(), "Hay errores en el formulario", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Cliente cli = new Cliente(nombre, apellidos, dni, direccion, telefono, email);
+                    clienteslist.add(cli);
+                    Intent intent = new Intent(view.getContext(), ClientesActivity.class);
+                    //intent.putExtra("nuevocliente", cli);
+                    intent.putExtra("listaclientes", clienteslist);
+                    startActivity(intent);
+                    finish();
+                }
                 break;
         }
+    }
+
+    private boolean validarEmail(String email)
+    {
+        //Regexp email
+        Pattern p = Pattern.compile("^[a-zA-Z0-9_-]{2,15}@[a-zA-Z0-9_-]{2,15}.[a-zA-Z]{2,4}(.[a-zA-Z]{2,4})?$");
+        Matcher m = p.matcher(email);
+        //si el correo es correcto devuelve TRUE o de lo contrario FALSE
+        return m.matches();
+    }
+
+    private boolean validarDNICIF(String dnicif)
+    {
+        boolean es_dni = false;
+        boolean es_cif = false;
+
+        //Regexp para dni
+        Pattern p = Pattern.compile("(([X-Z]{1})([-]?)(\\d{7})([-]?)([A-Z]{1}))|((\\d{8})([-]?)([A-Z]{1}))");
+        Matcher m = p.matcher(dnicif);
+
+        if (m.matches())
+        {
+            try
+            {
+                int dni = Integer.parseInt(dnicif.substring(0, dnicif.length()-1));
+                String juegoCaracteres="TRWAGMYFPDXBNJZSQVHLCKE";
+                int modulo= dni % 23;
+                char letra = juegoCaracteres.charAt(modulo);
+                char letra_formulario = dnicif.charAt(dnicif.length()-1);
+                if (letra == letra_formulario)
+                {
+                    es_dni = true;
+                }
+            }
+            catch (Exception e){
+                es_dni = false;
+            }
+        }
+
+        //Regexp para cif
+        p = Pattern.compile("^[a-zA-Z]{1}[0-9]{8}$");
+        m = p.matcher(dnicif);
+
+        es_cif = m.matches();
+
+        return es_dni || es_cif;
+    }
+
+    private boolean validarTFNO(String tfno)
+    {
+        boolean devolver=true;
+        try{
+            int i = Integer.parseInt(tfno);
+        } catch(Exception e) {
+           devolver=false;
+        }
+        return devolver;
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(getApplicationContext(), ClientesActivity.class);
+        intent.putExtra("listaclientes", clienteslist);
+        startActivity(intent);
+        finish();
     }
 }
